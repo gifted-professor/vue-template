c7fe81581eaeee9d67682c22a1a992dc30b63aac
gifted-professor <1044396185@qq.com>
2022-11-30 16:36:32 +0800

    init

---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+/*
+ * @desc: 
+ * @LastEditTime: 2022-11-30 16:31:51
+ * @params: 
+ * @return:: 
+ */
+module.exports = {
+	"*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
+	"{!(package)*.json,*.code-snippets,.!(browserslist)*rc}": ["prettier --write--parser json"],
+	"package.json": ["prettier --write"],
+	"*.vue": ["eslint --fix", "prettier --write", "stylelint --fix"],
+	"*.{scss,less,styl,html}": ["stylelint --fix", "prettier --write"],
+	"*.md": ["prettier --write"]
+};
